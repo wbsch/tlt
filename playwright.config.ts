4049300cc1c775ee42e019bc693e3bf610fb5fc2
Wilhelm Schürmann <wimschuermann@googlemail.com>
2026-03-01 21:25:03 +0100

    Reduce number of workers

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from '@playwright/test';
+import os from 'os';
+
+// Hacky, broken way of running num(physical_cores)-1 workers
+// If you have no hyperthreading I feel bad for you son
+const cpuCount = os.cpus().length / 2;
+const workers = Math.max(1, cpuCount - 1);
 
 export default defineConfig({
   testDir: './tests/e2e',
   timeout: 60_000,
+  workers: workers,
   expect: {
     timeout: 10_000,
   },
